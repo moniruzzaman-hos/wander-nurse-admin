f3485a0431aef1684a49f1e0dc9b906fbbc5c0a3
moniruzzaman-hos <moniruzzaman.bappi@healthos.se>
2024-04-21 00:50:32 +0600

    test

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Hello, world!</h1>
+      <h1 className="text-4xl font-bold">Hello, world!</h1>
+      <h1 className="text-4xl font-bold">Hello, world!</h1>
+      <h1 className="text-4xl font-bold">Hello, world!</h1>
       <>hi</>
     </main>
   );
